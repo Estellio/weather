b55ea4e7bbe7d014ee6524970c9347036918d51a
Estellio <pirko.vichmann@voco.ee>
2022-11-04 21:40:58 +0200

    Display weather data on the page

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-document.addEventListener('DOMContentLoaded', cityWeather)
+var form = document.querySelector('form')
+form.addEventListener('submit', weatherDataFetch)
 
-function weatherDataFetch (city) {
-    var key = 'fdec8be240d71f7b1eeb3f48ffd910df'
+function weatherDataFetch (e) {
+    var key = 'fdec8be240d71f7b1eeb3f48ffd910df';
+    var city = document.getElementById('city').value;
+    console.log(city)
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
         .then(function(resp) {
             return resp.json()
         })
         .then(function(data) {
             console.log(data);
+            drawWeather(data);
         })
         .catch(function() {
         });
+    e.preventDefault()
+    document.getElementById('city').value = ''
 }
 
-function cityWeather(e) {
-    weatherDataFetch('Tallinn');
+function drawWeather(data) {
+    var celsius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celsius + '&deg';
+    document.querySelector('#location').innerHTML = data.name;
+
 }
